659bdda54e0141116fc21eb759ee3ff3ede85d4c
Muskantyagi <65758130+Muskantyagi@users.noreply.github.com>
2020-05-22 18:50:20 +0530

    program4.py

---
+++ day 1/program 4.py
@@ -0,0 +1,6 @@
+sp = int(input("enter the selling price"))
+cp = int(input("enter the cost price"))
+profit = sp -cp
+print("the profit on commodity is :",profit)
+newsellp=0.05 * profit + cp
+print("after increasing profit by 5 %",newsellp)
